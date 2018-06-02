c44900f16badf36a6defe2a384bf7d1b07b85316
Oleg Teplitsky <arthadi@gmail.com>
2018-06-02 11:40:07 +0300

    and js

--- js/style_main-menu.js
+++ js/style_main-menu.js
@@ -1,1 +1,28 @@
-$(function(){var i=$(".js-hide-menu"),t=$(".menu__main");i.on("click",function(){if(0===t.height()){var i=t.css({height:"auto"}).outerHeight();return t.css({height:"0"}),t.animate({height:i},500),!1}return t.animate({height:0},500),!1}),$(window).on("resize",function(){551<$(window).width()&&t.attr("style"," ")})});
+$(function () {
+    var $button_hidden = $(".js-hide-menu");
+    var $menu_main = $(".menu__main");
+
+    $button_hidden.on("click", function () {
+
+        if ($menu_main.height() === 0) {
+            var height_menu = $menu_main.css({"height":"auto"}).outerHeight();
+            $menu_main.css({"height":"0"});
+            $menu_main.animate({"height": height_menu}, 500);
+            return false;
+        }
+        else {
+            $menu_main.animate({"height": 0}, 500);
+            return false;
+        }
+
+    });
+
+    $(window).on("resize", function () {
+        var width_window = $(window).width();
+
+        if (width_window > 551) {
+            $menu_main.attr("style", ' ');
+        }
+
+    });
+});
